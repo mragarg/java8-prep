304499c7f2d02eae5372b544392aa226cf46da04
Ashish Garg <mragarg@gmail.com>
2019-07-05 11:35:30 -0400

    AcmeOrderSystem Java Project

---
+++ AcmeOrderSystem/src/MyDate.java
@@ -0,0 +1,20 @@
+
+public class MyDate {
+
+	// Initialize time variables
+	int day, month, year;
+	
+	// Default Empty Constructor
+	public MyDate() {
+		
+	}
+	
+	// Constructor with arguments
+	public MyDate(int m, int d, int y) {
+		this.month = m;
+		this.day = d;
+		this.year = y;
+	}
+	
+	
+}
