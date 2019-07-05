9669b9a41c0077e348ec7ce9ebc2c98cf5fda3a5
Ashish Garg <mragarg@gmail.com>
2019-07-05 12:46:33 -0400

    Adds test cases

--- AcmeOrderSystem/src/TestOrders.java
+++ AcmeOrderSystem/src/TestOrders.java
@@ -14,6 +14,16 @@ public class TestOrders {
 		System.out.println(anvil);
 		System.out.println(balloons);
 
+		System.out.println("The tax Rate is currently: " + Order.taxRate);
+		Order.computeTaxOn(3000.00);
+		anvil.computeTax();
+		balloons.computeTax();
+		
+		Order.setTaxRate(0.06);
+		System.out.println("The tax Rate is currently: " + Order.taxRate);
+		Order.computeTaxOn(3000.00);
+		anvil.computeTax();
+		balloons.computeTax();
 	}
 
 }
