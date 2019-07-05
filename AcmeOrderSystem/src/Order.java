d9c2829d0a689cc986ef35f6977648712e19f96b
Ashish Garg <mragarg@gmail.com>
2019-07-05 12:37:51 -0400

    New function that computes that for an Order object

--- AcmeOrderSystem/src/Order.java
+++ AcmeOrderSystem/src/Order.java
@@ -19,11 +19,20 @@ public class Order {
 		return quantity + " ea. " + product + " for " + customer; 
 	}
 	
+	// Sets the taxRate with the newRate
 	public static void setTaxRate(double newRate) {
 		taxRate = newRate;
 	}
 	
+	// Returns a statement with the price with tax of an amount
 	public static void computeTaxOn(double anAmount) {
 		System.out.println("The tax for " + anAmount + " is: " + anAmount * Order.taxRate);
 	}
+	
+	// Computes tax for an Order Object
+	public double computeTax() {
+		System.out.println("The tax for this order is: " + orderAmount * Order.taxRate);
+		return orderAmount * Order.taxRate;
+	}
+	
 }
